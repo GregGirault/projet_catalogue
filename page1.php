fd0b9d1d4d2e9cd50158822379914bde07ba0544
greg0788 <g.girault@codeur.online>
2023-06-02 09:57:56 +0200

    page1

--- page1.php
+++ page1.php
@@ -6,20 +6,63 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style_page1.css">
-    <title>Ma Carte de Parfum</title>
+    <title>Rosalie Détour</title>
 </head>
 
 <body>
-    <h1>Rosalie Détour</h1>
-    <div class="card">
-        <div class="card-image">
-            <img src="./10.png" alt="Image de parfum">
+    <header>
+        <h1>Rosalie Détour</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Produits</a></li>
+                <li><a href="#">Catégories</a></li>
+                <li><a href="#">Contact</a></li>
+
+            </ul>
+        </nav>
+    </header>
+    <div class="card-container">
+        <div class="card">
+            <div class="card-image">
+                <img src="./10.png" alt="Image de parfum">
+            </div>
+            <div class="card-description">
+                <h2>Description</h2>
+                <p>Un parfum lumineux et énergisant qui capture l'éclat ensoleillé des pissenlits en plein épanouissement. Les
+                    notes de tête fruitées d'orange sanguine et de pêche apportent une douceur juteuse, tandis qu'un bouquet
+                    floral de pissenlit, de freesia et de magnolia ajoute une dimension florale élégante. Un fond de bois de
+                    santal et de musc offre une chaleur subtile et un sillage irrésistible.</p>
+            </div>
         </div>
-        <div class="card-description">
-            <h2>Description</h2>
-            <p>Un parfum lumineux et énergisant qui capture l'éclat ensoleillé des pissenlits en plein épanouissement. Les notes de tête fruitées d'orange sanguine et de pêche apportent une douceur juteuse, tandis qu'un bouquet floral de pissenlit, de freesia et de magnolia ajoute une dimension florale élégante. Un fond de bois de santal et de musc offre une chaleur subtile et un sillage irrésistible.</p>
+        <div class="slide-content ">
+            <h3>Ingrédients</h3>
+            <p>
+                Notre parfum Rosalie Détour est fabriqué avec des ingrédients de haute qualité, notamment :
+            </p>
+            <ul>
+                <li>Huiles essentielles naturelles</li>
+                <li>Extraits aromatiques concentrés obtenus à partir de matières premières naturelles</li>
+                <li>Notes de tête, de cœur et de fond soigneusement sélectionnées</li>
+                <li>Fixateurs pour assurer une tenue longue durée</li>
+                <li>Solvants respectueux de l'environnement</li>
+            </ul>
+            <p>Nous nous engageons à utiliser des ingrédients de la plus haute qualité pour créer une expérience parfumée
+                unique et envoûtante.</p>
         </div>
     </div>
+
+    <footer>
+        <h2>Rosalie Détour</h2>
+        <p>&copy; 2023 Carte de Parfum. Tous droits réservés.</p>
+        <nav>
+            <ul>
+                <li><a href="#">Politique de confidentialité</a></li>
+                <li><a href="#">Conditions d'utilisation</a></li>
+            </ul>
+        </nav>
+    </footer>
+
 </body>
 
 </html>
