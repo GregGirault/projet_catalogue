5706a97509d40e16a0696b04c99e5d7afe02343b
greg0788 <g.girault@codeur.online>
2023-06-02 15:34:19 +0200

    2 juin

--- page1.php
+++ page1.php
@@ -9,7 +9,9 @@
     <title>Rosalie Détour</title>
 </head>
 
-<body>
+<?php include 'header.php'; ?>
+
+<!-- <body>
     <header>
         <h1>Rosalie Détour</h1>
         <nav>
@@ -21,47 +23,50 @@
 
             </ul>
         </nav>
-    </header>
-    <div class="card-container">
-        <div class="card">
-            <div class="card-image">
-                <img src="./10.png" alt="Image de parfum">
-            </div>
-            <div class="card-description">
-                <h2>Description</h2>
-                <p>Un parfum lumineux et énergisant qui capture l'éclat ensoleillé des pissenlits en plein épanouissement. Les
-                    notes de tête fruitées d'orange sanguine et de pêche apportent une douceur juteuse, tandis qu'un bouquet
-                    floral de pissenlit, de freesia et de magnolia ajoute une dimension florale élégante. Un fond de bois de
-                    santal et de musc offre une chaleur subtile et un sillage irrésistible.</p>
-            </div>
+    </header> -->
+<!-- <h1>Rosalie Détour</h1> -->
+<div class="card-container">
+    <div class="card">
+        <div class="card-image">
+            <img src="./10.png" alt="Image de parfum">
         </div>
-        <div class="slide-content ">
-            <h3>Ingrédients</h3>
-            <p>
-                Notre parfum Rosalie Détour est fabriqué avec des ingrédients de haute qualité, notamment :
-            </p>
-            <ul>
-                <li>Huiles essentielles naturelles</li>
-                <li>Extraits aromatiques concentrés obtenus à partir de matières premières naturelles</li>
-                <li>Notes de tête, de cœur et de fond soigneusement sélectionnées</li>
-                <li>Fixateurs pour assurer une tenue longue durée</li>
-                <li>Solvants respectueux de l'environnement</li>
-            </ul>
-            <p>Nous nous engageons à utiliser des ingrédients de la plus haute qualité pour créer une expérience parfumée
-                unique et envoûtante.</p>
+        <div class="card-description">
+            <h2>Description</h2>
+            <p>Un parfum lumineux et énergisant qui capture l'éclat ensoleillé des pissenlits en plein épanouissement. Les
+                notes de tête fruitées d'orange sanguine et de pêche apportent une douceur juteuse, tandis qu'un bouquet
+                floral de pissenlit, de freesia et de magnolia ajoute une dimension florale élégante. Un fond de bois de
+                santal et de musc offre une chaleur subtile et un sillage irrésistible.</p>
         </div>
     </div>
+    <div class="slide-content ">
+        <h3>Ingrédients</h3>
+        <p>
+            Notre parfum Rosalie Détour est fabriqué avec des ingrédients de haute qualité, notamment :
+        </p>
+        <ul>
+            <li>Huiles essentielles naturelles</li>
+            <li>Extraits aromatiques concentrés obtenus à partir de matières premières naturelles</li>
+            <li>Notes de tête, de cœur et de fond soigneusement sélectionnées</li>
+            <li>Fixateurs pour assurer une tenue longue durée</li>
+            <li>Solvants respectueux de l'environnement</li>
+        </ul>
+        <p>Nous nous engageons à utiliser des ingrédients de la plus haute qualité pour créer une expérience parfumée
+            unique et envoûtante.</p>
+    </div>
+</div>
 
-    <footer>
-        <h2>Rosalie Détour</h2>
-        <p>&copy; 2023 Carte de Parfum. Tous droits réservés.</p>
-        <nav>
-            <ul>
-                <li><a href="#">Politique de confidentialité</a></li>
-                <li><a href="#">Conditions d'utilisation</a></li>
-            </ul>
-        </nav>
-    </footer>
+<?php include 'footer.php'; ?>
+
+<!-- <footer>
+    <h2>Rosalie Détour</h2>
+    <p>&copy; 2023 Carte de Parfum. Tous droits réservés.</p>
+    <nav>
+        <ul>
+            <li><a href="#">Politique de confidentialité</a></li>
+            <li><a href="#">Conditions d'utilisation</a></li>
+        </ul>
+    </nav>
+</footer> -->
 
 </body>
 
